941e7ef9708175ab5c7d2a72e28e695069e558d3
danielmcheng1 <dcheng21@uchicago.edu>
2017-09-17 18:08:47 -0700

    dynaminc flask moving to moba

--- static/scripts/imageviewer_navigate.js
+++ static/scripts/imageviewer_navigate.js
@@ -5,46 +5,49 @@ $(document).ready(function () {
         this_dates_images = [];
         for (int i = 0; i < this_dates_data.length; i++) {
             var image_info = '../static/images/' + this_dates_data["image_short_dir"] + "/" + this_dates_data["image_final"];
-            this_dates_images.push({small: image_info, large: image_info});
+            this_dates_images.push({small: image_info, large: image_info, timestamp: this_dates_data["hms"]});
             load_gallery(this_dates_images);
         }
     }
     
-    function show_image(ymd, currSpan){
-        var currIndex = gallery_image_index[ymd];
-        var image = images[currIndex];
-        viewer.load(image.small, image.big);
-        currSpan.html(currIndex + 1);
-    }
- 
-
-    function move_right(ymd, total) {
-        var nextIndex = gallery_image_index[ymd] + 1;
-        if(nextIndex >= total) nextIndex = 0;
-        show_image(ymd, currSpan);
-    }
-    function move_left(ymd, total) {
-        var nextIndex = gallery_image_index[ymd] - 1;
-        if (nextIndex < 0) gallery_image_index[ymd] = total - 1;
-        show_image(ymd, currSpan);
-    }
-    
-    function load_gallery(ymd, images) {   
-        var gallery_image_index[ymd] = 0;
-        var total = images.length;
-        
+    function show_image(images, ymd){
         var wrapper = $('#image-gallery-' + ymd);
         var currSpan = wrapper.find('.current');
         var viewer = ImageViewer(wrapper.find('.image-container'));
-     
+        var image = images[gallery_image_index[ymd]];
+        
+        viewer.load(image.small, image.big);
+        $("#timestamp-for-" + ymd).text(image.timestamp);
+        currSpan.html(gallery_image_index[ymd]);   
+    }
+ 
+
+    function move_right(images, ymd) {
+        var newIndex = gallery_image_index[ymd] + 1;
+        if (newIndex >= images.length) gallery_image_index[ymd] = 0;
+        else gallery_image_index[ymd] = newIndex;
+        
+        show_image(images, ymd);        
+    }
+    function move_left(images, ymd) {
+        var nextIndex = gallery_image_index[ymd] - 1;
+        if (nextIndex < 0) gallery_image_index[ymd] = images.length - 1;
+        
+        show_image(images, ymd);        
+    }
+    
+    function load_gallery(images, ymd) {   
+        var gallery_image_index[ymd] = 0;
+        
+        var wrapper = $('#image-gallery-' + ymd);
+        
         //display total count
-        wrapper.find('.total').html(total);
-     
-        wrapper.find('.next').click(moveRight(ymd));
-        wrapper.find('.prev').click(moveLeft(ymd));
+        wrapper.find('.total').html(images.length);     
+        wrapper.find('.next').click(moveRight(images, ymd));
+        wrapper.find('.prev').click(moveLeft(images, ymd));
         
         //initially show image
-        show_image(ymd, currSpan);
+        show_image(images, ymd);
         
         // playback / animation         
         var interval;
@@ -52,7 +55,7 @@ $(document).ready(function () {
             if ($(this).text().trim() == "Animate") {
                 $(this).text("Stop Animation");
                 interval = setInterval(function() {
-                    moveRight(ymd, total);
+                    moveRight(images, ymd);
                 }, 1000);
             } else {
                 clearInterval(interval);
